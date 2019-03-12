a6e7c789871503225938e32e6b4c0ac12a0e36ac
Matt Layher <mdlayher@gmail.com>
2019-03-12 09:22:36 -0400

    cronbuild: shell script to automatically rebuild and push the site

---
+++ .cronbuild.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -x
+
+cd "$(dirname "$0")"
+
+git pull
+
+# Regenerate static content and blog.
+go generate ./...
+hugo
+
+if [[ -z $(git status -s) ]]; then
+    # Nothing to do.
+    exit 0
+fi
+
+# Content was updated: push a new commit.
+git commit -am "mdlayher.com: automatic push of updated content"
+git push
